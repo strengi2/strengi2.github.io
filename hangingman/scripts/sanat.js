91040b4893a3d7c71750427f72fc27a439382bb5
strengi2 <oskari.streng@gmail.com>
2023-08-17 21:48:54 +0300

    i

---
+++ hangingman/scripts/sanat.js
@@ -0,0 +1,92 @@
+const wordList = [
+    {
+        word: "kitara",
+        hint: "ei apuja",
+    },
+    {
+        word: "rakas",
+        hint: "ei apuja",
+    },
+    {
+        word: "olio",
+        hint: "ei apuja",
+    },
+    {
+        word: "nopea",
+        hint: "ei apuja",
+    },
+    {
+        word: "viisas",
+        hint: "ei apuja",
+    },
+    {
+        word: "ankka",
+        hint: "ei apuja",
+    },
+    {
+        word: "pullo",
+        hint: "ei apuja",
+    },
+    {
+        word: "pilli",
+        hint: "ei apuja",
+    },
+    {
+        word: "mies",
+        hint: "ei apuja",
+    },
+    {
+        word: "nainen",
+        hint: "ei apuja",
+    },
+    {
+        word: "kiltti",
+        hint: "ei apuja",
+    },
+    {
+        word: "luotettava",
+        hint: "ei apuja",
+    },
+    {
+        word: "kaunis",
+        hint: "ei apuja",
+    },
+    {
+        word: "komea",
+        hint: "ei apuja",
+    },
+    {
+        word: "rummut",
+        hint: "ei apuja",
+    },
+    {
+        word: "kauppa",
+        hint: "ei apuja",
+    },
+    {
+        word: "tietokone",
+        hint: "ei apuja",
+    },
+    {
+        word: "koe",
+        hint: "ei apuja",
+    },
+    {
+        word: "koira",
+        hint: "ei apuja",
+    },
+    {
+        word: "kissa",
+        hint: "ei apuja",
+    },
+    {
+        word: "risti",
+        hint: "ei apuja",
+    },
+    {
+        word: "sana",
+        hint: "ei apuja",
+    },
+
+
+];
